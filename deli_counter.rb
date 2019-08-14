bb203b87fac05a0764f412a9bc6b03da436042d8
Mark Enright <enright.mark@gmail.com>
2019-08-14 20:20:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,63 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(array)
+  
+  if array == []
+   puts "The line is currently empty." 
+  else
+  temp_array = []
+  
+    array.each_with_index do |name, index|
+      
+      temp_array[index] = (index+1).to_s + ". " + name
+      
+    end
+    
+      puts "The line is currently: " + temp_array.join(" ")
+    
+    
+   
+    
+  end
+  
+end
+
+
+def take_a_number(array, name)
+  
+  array.push(name)
+  
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line." 
+end
+
+
+def now_serving(array)
+  
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
+
+line(katz_deli)
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
+
+line(katz_deli)
+now_serving(katz_deli)
+line(katz_deli)
+
+take_a_number(katz_deli, "Matz")
+line(katz_deli)
+now_serving(katz_deli)
+line(katz_deli)
+now_serving(katz_deli)
+now_serving(katz_deli)
+line(katz_deli)
+
+
+
---
+++ tester.rb
@@ -0,0 +1,4 @@
+x = ["what", "should", "something", "else"]
+
+print x.join(" ")
+print "does it make a space"
