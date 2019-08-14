6f7f4345d304af4b50e07fd5c2c1b28ce2649b4c
Mark Enright <enright.mark@gmail.com>
2019-08-14 20:03:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def take_a_number(array, name)
   
   array.push(name)
   
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line" 
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line." 
 end
 
 
