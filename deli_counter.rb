4cfdf3748febed17ac89b2cbe5b890326b8c3a4e
Mark Enright <enright.mark@gmail.com>
2019-08-14 19:43:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
   if array.length == 0
     "The line is currently empty."
   else
-    puts "The line is currently " + array.join
+    puts "The line is currently " + array.join(", ")
     
   end
   
